6206f8944d843759129e4c1a642aa42a4f9b2d96
Wagner Castro <wagner_gcastro@hotmail.com>
2023-11-04 16:59:50 -0300

    #04 - Create Module User and DTO

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,6 +6,7 @@ import * as dotenv from 'dotenv';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { getEnvPath } from './utils/helpers';
+import { UserModule } from './modules/user/user.module';
 
 dotenv.config({ path: path.join(__dirname, 'config', 'envs', `.env.${process.env.NODE_ENV}`) });
 
@@ -16,6 +17,7 @@ dotenv.config({ path: path.join(__dirname, 'config', 'envs', `.env.${process.env
       // expandVariables: true,
       envFilePath: getEnvPath(path.join(__dirname, 'config', 'envs ')),
     }),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
